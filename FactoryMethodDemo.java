f5155b6c1ff2ec8c5959083c09cd99194b565918
Akhil Chanda <52042860+akhilc668@users.noreply.github.com>
2020-11-02 17:51:23 +0530

    Add files via upload

---
+++ FactoryMethodDemo.java
@@ -0,0 +1,63 @@
+abstract class Mobile{
+abstract void cost();
+abstract void ram();
+abstract void harddisk();
+abstract void camera_pexcel();
+void open(){
+System.out.println("open with power botton");
+}
+}
+
+class Redmi extends Mobile{
+ void cost(){
+  System.out.println("Prize of Redmi is: 15000");
+ }
+ void ram(){
+  System.out.println("Ram of Redmi is: 6GB");
+ }
+ void harddisk(){
+  System.out.println("Harddisk of Redmi is: 128GB");
+ }
+ void camera_pexcel(){
+  System.out.println("camera_pexcel of Redmi is: 64px");
+ }
+}
+
+class Oneplus extends Mobile{
+ void cost(){
+  System.out.println("Prize of Redmi is: 45000");
+ }
+ void ram(){
+  System.out.println("Ram of Redmi is: 8GB");
+ }
+ void harddisk(){
+  System.out.println("Harddisk of Redmi is: 256GB");
+ }
+ void camera_pexcel(){
+  System.out.println("camera_pexcel of Redmi is: 45px");
+ }
+}
+
+class MobileFactory{
+ static Mobile getMobile(String mobileName){
+  if(mobileName.equalsIgnoreCase("Redmi")){
+   return new Redmi();
+  }
+  else if(mobileName.equalsIgnoreCase("Oneplus")){
+   return new Oneplus();
+  }
+  else {
+   return null;
+  }
+ }
+}
+class FactoryMethodDemo{
+public static void main(String args[]){
+Mobile x=MobileFactory.getMobile("Redmi");
+x.cost();
+x.ram();
+x.camera_pexcel();
+x.harddisk();
+x.open();
+}
+}
