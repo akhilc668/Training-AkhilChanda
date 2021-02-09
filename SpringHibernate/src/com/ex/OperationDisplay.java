fc898390ebde91ddb3ad5d6a8086a181a585c150
Akhil Chanda <52042860+akhilc668@users.noreply.github.com>
2021-02-09 23:44:42 +0530

    first commit

---
+++ SpringHibernate/src/com/ex/OperationDisplay.java
@@ -0,0 +1,56 @@
+package com.ex;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+
+public class OperationDisplay {
+	private HibernateTemplate hbTemplate;
+
+	public void setHbTemplate(HibernateTemplate hbTemplate) {
+		this.hbTemplate = hbTemplate;
+	}
+	
+	public void display(OperationDisplay sd) throws Exception {
+		Session se = sd.hbTemplate.getSessionFactory().openSession();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your choice \n 1.Retrieve All details\n 2. Retrieve sinlge row");
+		int choice = sc.nextInt();
+		if (choice == 1) {
+			Query qr = se.createQuery("from Employee e");
+			List<Employee> li = qr.list();
+			Iterator<Employee> i = li.iterator();
+			while (i.hasNext()) {
+				Employee e = i.next();
+				System.out.println("Employee [eid=" + e.getId() + ", name=" + e.getName() + ", salary=" + e.getSalary()
+						+ ", department=" + e.getDepartment() + ", designation=" + e.getDesignation() + ", address="
+						+ e.getAddress() + ", phone=" + e.getPhone() + ", DOJ=" + e.getDOJ() + "]");
+			}
+		} else if (choice == 2) {
+			Query qr = se.createQuery("from Employee e where e.id=:x");
+			String option = "";
+			do {
+				System.out.println("enter id of employee to be retrieve");
+				qr.setParameter("x", sc.nextInt());
+				List<Employee> li = qr.list();
+				Iterator<Employee> i = li.iterator();
+				while (i.hasNext()) {
+					Employee e = i.next();
+					System.out.println("Employee [eid=" + e.getId() + ", name=" + e.getName() + ", salary="
+							+ e.getSalary() + ", department=" + e.getDepartment() + ", designation="
+							+ e.getDesignation() + ", address=" + e.getAddress() + ", phone=" + e.getPhone() + ", DOJ="
+							+ e.getDOJ() + "]");
+				}
+				System.out.println("Do you want to continue (y/n): ");
+				option = sc.next();
+			} while (option.equalsIgnoreCase("y"));
+		}
+		se.close();
+	}
+
+}
---
+++ SpringHibernate/src/com/ex/OperationInsert.java
@@ -0,0 +1,52 @@
+package com.ex;
+
+
+import java.util.Scanner;
+
+
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class OperationInsert {
+	private HibernateTemplate hbTemplate;
+	
+	public void setHbTemplate(HibernateTemplate hbTemplate) {
+		this.hbTemplate = hbTemplate;
+	}
+
+	
+	public void insert(OperationInsert si) throws Exception {
+		Session se = si.hbTemplate.getSessionFactory().openSession();
+		Scanner sc = new Scanner(System.in);
+		String choice = "";
+		do {
+			Transaction tx = se.beginTransaction();
+			Employee e = new Employee();
+			System.out.println("enter employee id: ");
+			e.setId(sc.nextInt());
+			System.out.println("enter emploee name :");
+			e.setName(sc.next());
+			System.out.println("enter employee salary : ");
+			e.setSalary(sc.nextInt());
+			System.out.println("enter employee department :");
+			e.setDepartment(sc.next());
+			System.out.println("enter employee designation : ");
+			e.setDesignation(sc.next());
+			System.out.println("enter employee address: ");
+			e.setAddress(sc.next());
+			System.out.println("enter employee phone number : ");
+			e.setPhone(sc.next());
+			System.out.println("enter the date of Join :");
+			e.setDOJ(sc.next());
+			Object s = se.save(e);
+			tx.commit();
+			if (s != null) {
+				System.out.println("Record inserted");
+			}
+			System.out.println("Do you want to continue (y/n): ");
+			choice = sc.next();
+		} while (choice.equalsIgnoreCase("y"));
+		se.close();
+	}
+}
