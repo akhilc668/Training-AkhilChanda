cc0149ab4ce4a7e3a83a528be090329c5739c0d6
Akhil Chanda <52042860+akhilc668@users.noreply.github.com>
2021-02-24 13:19:54 +0530

    fouth commit

--- Angular/motivity/src/app/pages/employees/edit/edit.component.ts
+++ Angular/motivity/src/app/pages/employees/edit/edit.component.ts
@@ -10,34 +10,34 @@ import { EmployeeService } from 'src/app/services/employee.service';
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit {
-  id: number=0;
-  header:string="";
-  button:string="";
-  employee : Employee = {
-    id:0,
-    name:"",
-    email:"",
-    phone:0,
+  id: number = 0;
+  header: string = "";
+  button: string = "";
+  employee: Employee = {
+    id: 0,
+    name: "",
+    email: "",
+    phone: 0,
   };
-  constructor(private router: Router,private route: ActivatedRoute,private employeeservice:EmployeeService) { }
+  constructor(private router: Router, private route: ActivatedRoute, private employeeservice: EmployeeService) { }
 
   ngOnInit(): void {
-    this.id=+this.route.snapshot.params['id'];
-    this.header = this.id === 0 ? 'Add Employee': 'Edit Employee';
-    this.button = this.id === 0 ? 'save': 'update';
-    if(this.id === 0){
-      this.employee.id= this.employeeservice.latestId+1;
+    this.id = +this.route.snapshot.params['id'];
+    this.header = this.id === 0 ? 'Add Employee' : 'Edit Employee';
+    this.button = this.id === 0 ? 'save' : 'update';
+    if (this.id === 0) {
+      this.employee.id = this.employeeservice.latestId + 1;
     }
-    if(this.id != 0){
-      this.employee = <Employee> this.employeeservice.onGetEmployee(this.id);
+    if (this.id != 0) {
+      this.employee = <Employee>this.employeeservice.onGetEmployee(this.id);
     }
-    
-  
+
+
   }
 
-  
 
-  onSubmit(form: NgForm){
+
+  onSubmit(form: NgForm) {
     let employee: Employee = {
       id: form.value.id,
       name: form.value.name,
@@ -45,9 +45,9 @@ export class EditComponent implements OnInit {
       phone: form.value.phone,
     }
 
-    if(this.id === 0){
-     this.employeeservice.onAdd(employee);
-    }else{
+    if (this.id === 0) {
+      this.employeeservice.onAdd(employee);
+    } else {
       this.employeeservice.onUpdate(employee);
     }
     this.router.navigateByUrl('');
